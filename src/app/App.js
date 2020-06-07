dd65c601eed57c055bca7892b4ad2cc107eafafb
Imran Khan <imransilvake@gmail.com>
2020-06-07 15:29:59 +0200

    remove redux

--- src/app/App.js
+++ src/app/App.js
@@ -3,31 +3,19 @@ import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 
-// redux
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
 // app
 import AppRouter from './AppRouter';
-
-// redux store
-const store = createStore(() => {}, composeWithDevTools());
 
 class App extends Component {
 	render() {
 		return (
-			<Provider store={store}>
-				<BrowserRouter>
-					<React.Fragment>
-						{/* Header */}
+			<BrowserRouter>
+				{/* Header */}
 
-						<AppRouter/>
+				<AppRouter/>
 
-						{/* Footer */}
-					</React.Fragment>
-				</BrowserRouter>
-			</Provider>
+				{/* Footer */}
+			</BrowserRouter>
 		);
 	}
 }
